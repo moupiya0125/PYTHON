e30a3158a2260f874adb4338d072bf0c42484447
moupiya0125 <moupiya2002garai@gmail.com>
2025-05-14 08:41:54 +0530

    Learnt Randomization and list in python

---
+++ DAY4 Project/randomList.py
@@ -0,0 +1,9 @@
+import random
+
+my_list = ["A", "B", "C", "D", "E", "F" ]
+print(my_list[2])
+choice = random.choice(my_list)
+print(choice)
+
+random_index = random.randint(0,5)
+print(my_list[random_index])
