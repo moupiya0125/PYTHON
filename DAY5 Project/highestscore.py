c0f55a8dc5d3a64ca178fc905b23160ed077762d
moupiya0125 <moupiya2002garai@gmail.com>
2025-05-17 11:42:50 +0530

    Added DAY 5

---
+++ DAY5 Project/highestscore.py
@@ -0,0 +1,8 @@
+student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
+print(max(student_scores))
+
+maximum = student_scores[0]
+for score in student_scores:
+    if maximum < score:
+        maximum = score
+print(maximum)    
